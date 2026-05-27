dea120d057ed135330e149beb2dc0783d89bab34
Yash021205 <145587676+Yash021205@users.noreply.github.com>
2026-05-27 23:59:55 +0530

    Add Binary Search

---
+++ Binary Search/BS.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int BinarySearch(vector<int> nums,int left,int right, int target){
+        int mid = left + (right - left) / 2;
+         if(left > right) return -1;
+        if(nums[mid]==target) return mid;
+        else if(target> nums[mid]){
+            return BinarySearch(nums,mid+1,right,target);
+        }
+        else{
+            return BinarySearch(nums,left,mid-1,target);
+        }
+      
+    }
+    int search(vector<int>& nums, int target) {
+        return BinarySearch(nums,0,nums.size()-1,target);
+    }
+};
