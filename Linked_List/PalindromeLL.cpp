b6e7e71163e2c4dbf7222752845de4861f96e281
Yash021205 <145587676+Yash021205@users.noreply.github.com>
2026-06-01 23:20:39 +0530

    Add LC LL Question

---
+++ Linked_List/PalindromeLL.cpp
@@ -0,0 +1,44 @@
+// Palindrome Linked List
+//TC: O(N)
+//SC: O(1)
+//LC 234
+
+#include <iostream>
+using namespace std;
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* newhead =reverse(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newhead;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(!head || head->next==NULL) return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newhead=reverse(slow->next);
+        ListNode* first=head;
+        ListNode* second=newhead;
+        while(second!=NULL){
+            if(first->val!=second->val) return false;
+            first=first->next;
+            second=second->next;
+        }
+        return true;
+    }
+};
